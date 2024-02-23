a819732ea3bb68843ded7fe5e21c5544130303ce
anzo-p <anssi.pitkan@gmail.com>
2024-02-23 11:41:47 +0200

    bakcend - validation

--- backend/src/main/scala/net/anzop/dto/TrackMetadataDto.scala
+++ backend/src/main/scala/net/anzop/dto/TrackMetadataDto.scala
@@ -3,16 +3,16 @@ package net.anzop.dto
 import cats.data._
 import cats.implicits._
 import net.anzop.audiostreamer.{AddTrackMetadataInput, TrackMetadataOutput}
-import net.anzop.models.{TrackMetadata, TrackMetadataParams}
+import net.anzop.models.TrackMetadata
 import net.anzop.services.InvalidObject
+
+import java.time.LocalDate
+import java.util.UUID
 
 object TrackMetadataDto {
 
   def toModel(dto: AddTrackMetadataInput): Either[InvalidObject, TrackMetadata] =
-    validate(dto).toEither match {
-      case Left(invalid) => Left(InvalidObject(invalid.toList.mkString(", ")))
-      case Right(params) => TrackMetadata.make(params)
-    }
+    validate(dto).toEither.leftMap(invalid => InvalidObject(invalid.toList.mkString(", ")))
 
   def fromModel(model: TrackMetadata): TrackMetadataOutput = {
     TrackMetadataOutput(
@@ -29,16 +29,44 @@ object TrackMetadataDto {
     )
   }
 
-  def validate(input: AddTrackMetadataInput): ValidatedNel[InvalidObject, TrackMetadataParams] =
+  private def validateBitrate(bitrate: Option[Int]): ValidatedNel[InvalidObject, Option[Int]] =
+    bitrate match {
+      case Some(b) if b <= 0     => InvalidObject("Invalid bitrate").invalidNel
+      case Some(b) if b > 400000 => InvalidObject("Bitrate too high").invalidNel
+      case _                     => bitrate.validNel
+    }
+
+  private def validateFileSize(fileSize: Int): ValidatedNel[InvalidObject, Int] =
+    fileSize match {
+      case size if size <= 0        => InvalidObject("Invalid file size").invalidNel
+      case size if size > 100000000 => InvalidObject("File size too large").invalidNel
+      case _                        => fileSize.validNel
+    }
+
+  private def validateFormat(format: String): ValidatedNel[InvalidObject, String] =
+    format.toLowerCase() match {
+      case "mp3" | "flac" | "ogg" | "wav" => format.validNel
+      case _                              => InvalidObject("Invalid format").invalidNel
+    }
+
+  private def validateYear(year: Option[Int]): ValidatedNel[InvalidObject, Option[Int]] =
+    year match {
+      case Some(y) if y < 1900                    => InvalidObject("Invalid year").invalidNel
+      case Some(y) if y > LocalDate.now().getYear => InvalidObject("Year cannot be in the future").invalidNel
+      case _                                      => year.validNel
+    }
+
+  private def validate(input: AddTrackMetadataInput): ValidatedNel[InvalidObject, TrackMetadata] =
     (
       input.album.validNel,
       input.artist.validNel,
-      input.bitrate.validNel,
+      validateBitrate(input.bitrate),
       input.duration.validNel,
-      input.fileSize.validNel,
-      input.format.validNel,
+      validateFileSize(input.fileSize),
+      validateFormat(input.format),
       input.genre.validNel,
       input.title.validNel,
-      input.year.validNel
-    ).mapN(TrackMetadataParams)
+      UUID.randomUUID().toString.validNel,
+      validateYear(input.year)
+    ).mapN(TrackMetadata)
 }
--- backend/src/main/scala/net/anzop/models/TrackMetadata.scala
+++ backend/src/main/scala/net/anzop/models/TrackMetadata.scala
@@ -1,10 +1,6 @@
 package net.anzop.models
 
-import net.anzop.services.InvalidObject
-
-import java.util.UUID
-
-final case class TrackMetadataParams(
+final case class TrackMetadata(
     album: Option[String] = None,
     artist: String,
     bitrate: Option[Int] = None,
@@ -13,38 +9,6 @@ final case class TrackMetadataParams(
     format: String,
     genre: List[String],
     title: String,
-    year: Option[Int] = None
-  )
-
-final case class TrackMetadata(
-    artist: String,
-    album: Option[String] = None,
-    bitrate: Option[Int]  = None,
-    duration: Int,
-    fileSize: Int,
-    format: String,
-    genre: List[String],
-    title: String,
     trackId: String,
     year: Option[Int] = None
   )
-
-object TrackMetadata {
-
-  def make(params: TrackMetadataParams): Either[InvalidObject, TrackMetadata] = {
-    val a = TrackMetadata(
-      artist   = params.artist,
-      album    = params.album,
-      bitrate  = params.bitrate,
-      duration = params.duration,
-      genre    = params.genre,
-      fileSize = params.fileSize,
-      format   = params.format,
-      title    = params.title,
-      trackId  = UUID.randomUUID().toString,
-      year     = params.year
-    )
-
-    Right(a)
-  }
-}
