90e3a20c1f8137d0bd36990474a78374ed22d170
anzo-p <anssi.pitkan@gmail.com>
2024-02-25 15:32:20 +0200

    backend - save file sync

--- backend/src/main/scala/net/anzop/services/FileService.scala
+++ backend/src/main/scala/net/anzop/services/FileService.scala
@@ -1,14 +1,18 @@
 package net.anzop.services
 
 import cats.effect._
+import cats.effect.kernel.Sync
 import cats.implicits._
 import fs2.io.file.Flag._
 import fs2.io.file.{Files, Flags, Path}
 import org.http4s.multipart.Part
+import smithy4s.Blob
+
+import java.nio.file.{Files => JFiles, Path => JPath, Paths => JPaths}
 
 class FileService[F[_] : Async] {
 
-  def saveFile(trackId: String, filePart: Part[F]): F[Path] = {
+  def saveAsync(trackId: String, filePart: Part[F]): F[Path] = {
     val trackDir = Path(s"uploadedFiles/$trackId")
     val fileName = filePart.filename.getOrElse("default.txt")
     val path     = trackDir.resolve(fileName)
@@ -17,5 +21,19 @@ class FileService[F[_] : Async] {
       _ <- Files[F].createDirectories(trackDir)
       _ <- filePart.body.through(Files[F].writeAll(path, Flags(Write, Create, Truncate))).compile.drain
     } yield path
+  }
+
+  def saveSync(trackId: String, fileName: String, fileBlob: Blob): F[JPath] = {
+    val trackDir = JPaths.get(s"uploadedFiles/$trackId")
+    val path     = trackDir.resolve(fileName)
+
+    Sync[F]
+      .blocking {
+        if (!JFiles.exists(trackDir)) {
+          JFiles.createDirectories(trackDir)
+        }
+        JFiles.write(path, fileBlob.toArray)
+      }
+      .as(path)
   }
 }
--- backend/src/main/scala/net/anzop/services/TrackService.scala
+++ backend/src/main/scala/net/anzop/services/TrackService.scala
@@ -4,10 +4,11 @@ import cats.data.EitherT
 import cats.effect._
 import cats.implicits._
 import doobie.util.transactor.Transactor
-import net.anzop.audiostreamer.AddTrackMetadataInput
+import net.anzop.audiostreamer._
 import net.anzop.db.DbOps
 import net.anzop.dto.TrackMetadataDto
 import org.http4s.multipart.Part
+import smithy4s.Blob
 
 class TrackService[F[_] : Async](implicit xa: Transactor[F], fs: FileService[F], db: DbOps[F]) {
 
@@ -23,7 +24,17 @@ class TrackService[F[_] : Async](implicit xa: Transactor[F], fs: FileService[F],
   def uploadTrackFile(trackId: String, file: Part[F]): F[ServiceResult] = {
     val operation = for {
       _    <- EitherT(db.getTrackMetadata(trackId).map(_.leftMap(ServiceError.handle)))
-      path <- EitherT.right(fs.saveFile(trackId, file))
+      path <- EitherT.right(fs.saveAsync(trackId, file))
+      _    <- EitherT(db.updateFilepath(trackId, path.toString).map(_.leftMap(ServiceError.handle)))
+    } yield SuccessResult()
+
+    operation.value.map(_.merge)
+  }
+
+  def uploadTrackFile(trackId: String, filename: String, file: Blob): F[ServiceResult] = {
+    val operation = for {
+      _    <- EitherT(db.getTrackMetadata(trackId).map(_.leftMap(ServiceError.handle)))
+      path <- EitherT.right(fs.saveSync(trackId, filename, file))
       _    <- EitherT(db.updateFilepath(trackId, path.toString).map(_.leftMap(ServiceError.handle)))
     } yield SuccessResult()
 
