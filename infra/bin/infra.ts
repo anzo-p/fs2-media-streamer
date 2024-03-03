c0683f04ff4d38d7c2eb27e145ac3764056c672e
anzo-p <anssi.pitkan@gmail.com>
2024-03-03 08:09:44 +0200

    infra - vpc and aurora

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -1,15 +1,32 @@
 #!/usr/bin/env node
-import "source-map-support/register";
-import * as cdk from "aws-cdk-lib";
-import { InfraStack } from "../lib/infra-stack";
-import * as dotenv from "dotenv";
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { AuroraStack } from '../lib/aurora-stack';
+import * as dotenv from 'dotenv';
 
 dotenv.config();
 
+let deployed = false;
+
 const app = new cdk.App();
-new InfraStack(app, "InfraStack", {
-  env: {
-    account: process.env.AWS_ACCOUNT,
-    region: process.env.AWS_REGION,
-  },
+const args = process.argv.slice(2);
+const env = {
+  account: process.env.AWS_ACCOUNT,
+  region: process.env.AWS_REGION
+};
+
+const stacks: { [key: string]: () => void } = {
+  StackDB: () => new AuroraStack(app, 'AuroraStack', { env })
+};
+
+Object.keys(stacks).forEach((stackName) => {
+  if (args.includes(stackName)) {
+    stacks[stackName]();
+    deployed = true;
+  }
 });
+
+if (!deployed) {
+  console.error('Error: No stack specified for deployment. Please specify a stack name.');
+  process.exit(1);
+}
--- infra/lib/aurora-stack.ts
+++ infra/lib/aurora-stack.ts
@@ -1,38 +1,61 @@
-import * as cdk from "aws-cdk-lib";
-import * as ec2 from "aws-cdk-lib/aws-ec2";
-import * as rds from "aws-cdk-lib/aws-rds";
-import { Construct } from "constructs";
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as rds from 'aws-cdk-lib/aws-rds';
 
-export class AuroraPostgresStack extends cdk.NestedStack {
-  constructor(
-    scope: Construct,
-    id: string,
-    vpc: ec2.Vpc,
-    props?: cdk.NestedStackProps
-  ) {
+export class AuroraStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const vpc = new ec2.Vpc(this, 'AudioStreamerVpc', {
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+          name: 'public',
+          subnetType: ec2.SubnetType.PUBLIC
+        },
+        {
+          name: 'isolated',
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED
+        }
+      ],
+      natGateways: 0
+    });
+
+    const auroraSg = new ec2.SecurityGroup(this, 'AuroraSecurityGroup', {
+      vpc
+    });
+
     const credentials = rds.Credentials.fromPassword(
-      process.env.AURORA_USERNAME!,
-      cdk.SecretValue.unsafePlainText(process.env.AURORA_PASSWORD!)
+      process.env.DB_USERNAME!,
+      cdk.SecretValue.unsafePlainText(process.env.DB_PASSWORD!)
     );
 
-    new rds.ServerlessCluster(this, "AudioStreamerAuroraPostgres", {
+    new rds.ServerlessCluster(this, 'AudioStreamerAuroraPostgres', {
       credentials,
       engine: rds.DatabaseClusterEngine.auroraPostgres({
-        version: rds.AuroraPostgresEngineVersion.VER_13_10,
+        version: rds.AuroraPostgresEngineVersion.VER_13_10
       }),
-      defaultDatabaseName: "audio_streamer",
+      defaultDatabaseName: `${process.env.DB_NAME}`,
       removalPolicy: cdk.RemovalPolicy.RETAIN,
       scaling: {
         autoPause: cdk.Duration.minutes(5),
         minCapacity: rds.AuroraCapacityUnit.ACU_8,
-        maxCapacity: rds.AuroraCapacityUnit.ACU_32,
+        maxCapacity: rds.AuroraCapacityUnit.ACU_32
       },
+      securityGroups: [auroraSg],
       vpc,
       vpcSubnets: {
-        subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
-      },
+        subnetType: ec2.SubnetType.PRIVATE_ISOLATED
+      }
+    });
+
+    new cdk.CfnOutput(this, 'ExportedVpcId', {
+      value: vpc.vpcId
+    });
+
+    new cdk.CfnOutput(this, 'ExportedAuroraSecurityGroupId', {
+      value: auroraSg.securityGroupId
     });
   }
 }
