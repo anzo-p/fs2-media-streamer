e4add5f9978f8c55c11be0b552d6ca96a9660517
anzo-p <anssi.pitkan@gmail.com>
2024-03-04 12:08:52 +0200

    crates - allowed cors origins via ENV

--- crates/src/main/scala/net/anzop/config/CorsPolicy.scala
+++ crates/src/main/scala/net/anzop/config/CorsPolicy.scala
@@ -1,5 +1,6 @@
 package net.anzop.config
 
+import net.anzop.helpers.Extensions.EnvOps
 import org.http4s.headers.Origin
 import org.http4s.server.middleware.{CORS, CORSPolicy}
 import org.http4s.{Method, Uri}
@@ -7,13 +8,19 @@ import org.typelevel.ci.CIStringSyntax
 
 object CorsPolicy {
 
+  private val corsOrigins: Seq[String] =
+    sys.env.getOrThrow("CORS_ALLOWED_ORIGINS", "CORS_ALLOWED_ORIGINS is not set").split(",")
+
+  private val allowedOrigins: PartialFunction[Origin.Host, Boolean] = {
+    case Origin.Host(Uri.Scheme.http | Uri.Scheme.https, Uri.RegName(host), _) if corsOrigins.contains(host.toString.toLowerCase) =>
+      true
+    case _ =>
+      false
+  }
+
   val config: CORSPolicy = CORS
     .policy
-    .withAllowOriginHost({
-      case Origin.Host(Uri.Scheme.http, Uri.RegName(ci"localhost"), Some(3000))  => true
-      case Origin.Host(Uri.Scheme.https, Uri.RegName(ci"localhost"), Some(3000)) => true
-      case _                                                                     => false
-    })
+    .withAllowOriginHost(allowedOrigins)
     .withAllowMethodsIn(Set(Method.GET, Method.POST))
     .withAllowHeadersIn(Set(ci"Content-Type"))
     .withAllowCredentials(false)
